30f9aeee3b92731858715b012de1a5b12af59a46
macthgamesfree <barbra398@gmail.com>
2026-03-23 18:51:37 +0700

    add

---
+++ knife_hit_v1/adtag-vast.js
@@ -0,0 +1,8 @@
+// adtag-vast.js
+// Chứa mã ad tag VAST cho SDK IMA hoặc các hệ thống quảng cáo video
+
+// Ví dụ: Chỉ là URL hoặc đoạn mã cấu hình, tuỳ hệ thống tích hợp
+var vastAdTagUrl = "https://pubads.g.doubleclick.net/gampad/ads?iu=/23136362493/ads-video-main&description_url=http%3A%2F%2Fcool2fun.guthub.io&tfcd=0&npa=0&sz=640x480&gdfp_req=1&unviewed_position_start=1&output=vast&env=vp&impl=s&correlator=";
+
+// Nếu cần, bạn có thể export hoặc gắn vào window để sử dụng ở các file khác
+window.vastAdTagUrl = vastAdTagUrl;
